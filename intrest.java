5e9a10024146625ddf484fa500b95fa9a4e31178
Tania <112808747+Taniazain@users.noreply.github.com>
2023-01-11 23:54:43 +0530

    Add files via upload

---
+++ intrest.java
@@ -0,0 +1,27 @@
+impact.java.utill.scanner
+class siint{
+class double si(int p,double r,int t){
+double i;
+i=(p*r*t)100;
+return(i);
+}}
+class intrest{
+public Static void main(String args[])}
+Scanner sc=new scanner(System.in);
+sint obj=new siint();
+inta,y;
+double n;
+System.out.print("enter the principal amount");
+a=sc.nextlnt();
+system.out.print("Enter the number of years");
+y=sc.nextln();
+char c;
+system.out.print("Is person a senior citizen.");
+c=sc.next().charAt(0);
+n=0.10;
+if(c=='y')
+n=0.12;
+System.out.print("intrest:"+(obj.si(a,n,y)));
+}
+}
+ 
